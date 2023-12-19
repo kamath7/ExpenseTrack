eb79f16ea0559fa9d2c555f0ff8b61595a780db3
kamath7 <adithyakamath96@gmail.com>
2023-12-20 01:39:40 +0530

    add expense started

--- src/hooks/useAddTransaction.js
+++ src/hooks/useAddTransaction.js
@@ -1,14 +1,18 @@
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db } from "../config/firebase-config";
+import { useGetUserInfo } from "./useGetUserInfo";
+
 export const useAddTransaction = () => {
   const transactionCollection = collection(db, "transactions");
+  const { userID } = useGetUserInfo();
+
   const addTransaction = async ({
     description,
     transactionAmount,
     transactionType,
   }) => {
     await addDoc(transactionCollection, {
-      userID: "",
+      userID,
       description,
       transactionAmount,
       transactionType,
