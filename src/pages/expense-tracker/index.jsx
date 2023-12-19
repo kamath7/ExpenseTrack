83731bf305dfe8f27879d7ad44601041c1289a4a
kamath7 <adithyakamath96@gmail.com>
2023-12-20 00:38:50 +0530

    Added expense tracker boiler code

--- src/pages/expense-tracker/index.jsx
+++ src/pages/expense-tracker/index.jsx
@@ -1,5 +1,36 @@
 import React from "react";
 
 export const ExpenseTracker = () => {
-  return <div>ExpenseTracker</div>;
+  return (
+    <>
+      <div className="expense-tracker">
+        <div className="container">
+          <h1>Expense Tracker</h1>
+          <div className="balance">
+            <h2>Your balance</h2>
+            <h2>₹0.00</h2>
+          </div>
+          <div className="summary">
+            <div className="income">
+              <h4>Income</h4>
+              <p>₹0.00</p>
+            </div>
+            <div className="expenses">
+              <h4>Expenses</h4>
+              <p>₹0.00</p>
+            </div>
+          </div>
+          <form className="add-transaction">
+            <input type="text" placeholder="Description" required />
+            <input type="number" placeholder="Amount" required />
+            <input type="radio" id="expense" value="expense" />
+            <label htmlFor="expense">Expense</label>
+            <input type="radio" id="income" value="income" />
+            <label htmlFor="income">Income</label>
+            <button type="submit">Add Transaction</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
