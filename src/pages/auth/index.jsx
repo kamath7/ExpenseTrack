118c754f13d8ed08a3ec5a35e396da47df0f46a1
kamath7 <adithyakamath96@gmail.com>
2023-12-21 02:05:42 +0530

    Navigating

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { auth, provider } from "../../config/firebase-config";
 import { signInWithPopup } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
 import "./styles.css";
+import { useGetUserInfo } from "../../hooks/useGetUserInfo";
 export const Auth = () => {
   const navigate = useNavigate();
+  const { isAuth } = useGetUserInfo();
   const signInWithGoogle = async () => {
     const result = await signInWithPopup(auth, provider);
     const authInfo = {
@@ -17,6 +19,10 @@ export const Auth = () => {
     localStorage.setItem("auth", JSON.stringify(authInfo));
     navigate("/expense-tracker");
   };
+
+  if (isAuth) {
+    return <Navigate to="/expense-tracker" />;
+  }
   return (
     <div className="login-page">
       <p>Sign In with Google</p>
